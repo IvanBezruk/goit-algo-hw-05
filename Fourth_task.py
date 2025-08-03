b32d7ccf5b68b6d8d61ceba956795e18f3393583
Iva <ivanbezruk2022@gmail.com>
2025-08-03 18:51:49 +0200

    fourth_task

---
+++ Fourth_task.py
@@ -0,0 +1,58 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, *kwargs)
+        except KeyError:
+            return "Contact not found"
+        except ValueError:
+            return "Enter the argument for the command"
+    return inner
+
+@input_error
+def add_contact(args, contacts):
+    if len(args) < 2:
+        raise ValueError
+    name, phone = args[0], args[1]
+    contacts[name] = phone
+    return "Contact added"
+
+@input_error
+def show_phone(args, contacts):
+    if len(args)<1:
+        raise IndexError
+    name = args[0]
+    if name not in contacts:
+        raise KeyErrorreturn f"{name}: {contacts[name]}"
+    return f"{name}: {contacts[name]}"
+
+@input_error
+def show_all(args, contacts):
+    if not contacts:
+        return "No contacts found"
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    return cmd.lower(), args
+
+def main():
+    contact = {}
+    while True:
+        user_input = input("Enter a command: ").strip()
+        if not user_input:
+            continue
+        command, args = parse_input(user_input)
+        if command in ("close", "exit", "quit"):
+            print("Good bye")
+            break
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(args, contacts))
+        else:
+            print("Invalid command.")
+
+if __name__=="__main__":
+    main()
--- base.py
+++
@@ -1,6 +0,0 @@
-parse_log_line(line: str) -> dict
-load_logs(file_path: str) -> list
-filter_logs_by_level(logs: list, level: str) -> list
-count_logs_by_level(logs: list) -> dict
-display_log_counts(counts: dict)
-display_filtered_logs(logs: list, level: str)
